193e38d2064b87244296f5a0b1474f25d57fe2ba
Yixbro <yixmanny9000@gmail.com>
2025-03-14 00:03:11 +0100

    auth integration process almost completed

--- src/components/login[signUp]/signUp.jsx
+++ src/components/login[signUp]/signUp.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router";
 import { FiEye, FiEyeOff } from "react-icons/fi";
 import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
-import { loginSuccess, logout } from '../../store/authSlice';
+import { loginSuccess } from '../../store/authSlice';
+import useAuth from "../hooks/useAuth";
+import displayError from "../utils/displayError";
 
-const editNumber = (num) => {
+const apiUrl = import.meta.env.VITE_API_URL;
+
+const formatPhone = (num) => {
   const split = num.toString().split('')
   split.shift(); split.unshift('+234')
   const newNum = split.join('');
@@ -15,59 +19,81 @@ const editNumber = (num) => {
 }
 
 const SignUp = () => {
-  const URL = 'https://running-arlie-nexusmind-b9a0fcb2.koyeb.app/auth/users'
-
   const [showPassword, setShowPassword] = useState(false);
-  const [error, setError] = useState([]);
+  // const [error, setError] = useState([]);
 
   //User fields input
   const [password, setPassword] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('')
   const [username, setUsername] = useState('')
-  const [sucessLoginMsg, setSucessLoginMsg] = useState(false)
 
   const isAuth = useSelector((state) => state.auth)
   const dispatch = useDispatch();
+
+  const { login, signup, loading, error, data } = useAuth();
 
   //handleSubmit
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (!username || !phoneNumber || !password) {
-      setError('All input fields must be filled')
-      return;
-    }
+    // if (!username || !phoneNumber || !password) {
+    //   setError([{ others: 'All input must be filled' }])
+    //   return;
+    // }
 
-    console.log(username, phoneNumber, password)
-    handleSubmitAsync(username, editNumber(phoneNumber), password);
+    // console.log(username, phoneNumber, password)
+    // handleSubmitAsync(username, editNumber(phoneNumber), password);
 
+    signup(apiUrl + "/auth/users", { username, phone_number: formatPhone(phoneNumber), password })
   }
 
-  const handleSubmitAsync = async (user, phone, pass) => {
-    setError([]);
-    setSucessLoginMsg(false)
-
-
-    try {
-      const request = await axios.post(URL, { username: user, phone_number: phone, password: pass })
-      const response = request.data
-      if (response.username) {
-        setSucessLoginMsg(true)
-        localStorage.setItem('username', response.username)
-
-      }
-
-    } catch (err) {
-      if (err.status == 400) {
-        // setError(err.response.data.phone_number[0])
-        const error = err.response.data
-        // setError()
-        console.log(error)
-      } else {
-        console.log(err)
-      }
+  useEffect(() => {
+    if (data !== null) {
+      dispatch(loginSuccess({ token: data }))
     }
-  }
+  }, [data])
+
+
+  // //handling signup auth;
+  // const handleSubmitAsync = async (user, phone, pass) => {
+  //   setError([]);
+  //   setSucessLoginMsg(false)
+
+
+  //   try {
+  //     const request = await axios.post(apiUrl + 'auth/users/', { username: user, phone_number: phone, password: pass })
+  //     const response = { user, phone, pass }
+  //     if (response.username) {
+  //       setSucessLoginMsg(true)
+  //       localStorage.setItem('username', response.username)
+  //     }
+
+  //     // cb(user, phone)
+
+  //   } catch (err) {
+  //     if (err.status == 400) {
+  //       setError((prevState) => [...prevState, err.response.data])
+  //       const error = err.response.data
+  //       // setError()
+  //       console.log(error)
+  //     } else {
+  //       setError([{ others: 'Network issue, TRY AGAIN !!!' }])
+  //     }
+  //   }
+  // }
+
+  //login after a user is signup
+  // const login = async (user, pass) => {
+
+  //   try {
+  //     const request = await axios.post(apiUrl + '/auth/token/', { username: user, password: pass })
+  //     const response = request.data;
+  //     cconsole.log(response)
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }
+
   return (
     <section className="mt-10 flex flex-col gap-7 px-7 min-h-screen max-w-3xl mx-auto">
       <h2 className="font-poppins font-bold text-black text-xl/[30px]">
@@ -76,22 +102,31 @@ const SignUp = () => {
 
       <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
         {/* username  input */}
-        <input
-          className="input rounded-none pt-0 h-[46px]"
-          type="text"
-          name="username"
-          placeholder="Username"
-          onChange={(e) => setUsername(e.target.value)}
-        />
+        <div className="flex flex-col">
+          <input
+            className="input rounded-none pt-0 h-[46px]"
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <p className="text-red-500 font-bold">{displayError(error, 'username')}</p>
+        </div>
+
+
 
         {/* Phone number Input */}
-        <input
-          className="input rounded-none pt-0 h-[46px]"
-          type="number"
-          name="number"
-          placeholder="Phone Number"
-          onChange={(e) => setPhoneNumber(e.target.value)}
-        />
+        <div className="flex flex-col">
+          <input
+            className="input rounded-none pt-0 h-[46px]"
+            type="number"
+            name="number"
+            placeholder="Phone Number"
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+          <p className="text-red-500 font-bold">{displayError(error, 'phone_number')}</p>
+        </div>
+
 
 
         {/* Password Input with Toggle Icon */}
@@ -113,15 +148,15 @@ const SignUp = () => {
           </button>
         </div>
 
+        {/* error msg */}
+        <p className="text-red-500 font-bold">{displayError(error, 'others')}</p>
         {/* Register Button */}
-        <p className="text-red-500 font-bold">{error}</p>
-        {sucessLoginMsg && <p className="text-green-500"><span className="font-bold">{username}</span> You've successfully created an account </p>}
 
         <button
           type="submit"
           className="input pt-0 h-[46px] bg-sun border-none rounded-[3px] font-inter font-bold text-[15px]/[18.51px]"
         >
-          Register
+          {loading ? "Loading ..." : 'Register'}
         </button>
       </form>
 
