93215f21b2d16ae6832b69cade0904ceaaf7094d
Mkzay <80535194+Mkzay@users.noreply.github.com>
2025-03-19 20:49:46 +0100

    createShop, searchResults

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -198,7 +198,7 @@ const Header = () => {
                   <div className="flex items-center">
                     {shop.image && (
                       <img
-                        src={shop.image}
+                        src={shop.image_url}
                         alt={shop.name}
                         className="w-10 h-10 object-cover mr-3"
                       />
