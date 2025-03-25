4669c18ac760039216d2e02abcc58dd6755d7b30
Mkzay <80535194+Mkzay@users.noreply.github.com>
2025-03-25 21:59:21 +0100

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 500, // (Optional) Adjust warning limit
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Separate vendor dependencies
+          }
+        },
+      },
+    },
+  },
+});
