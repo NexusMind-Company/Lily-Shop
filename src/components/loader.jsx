8ec0a66f7a77e23e85cdd094ecbfa784c6da7d4d
Mkzay <80535194+Mkzay@users.noreply.github.com>
2025-03-28 11:27:26 +0100

    ...

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="flex absolute top-[50%] right-[50%] items-center justify-center">
+    <div className="flex absolute top-[60%] right-[40%] md:top-[50%]  md:right-[50%] items-center justify-center">
       <div className="flex space-x-2">
         <div className="w-3 h-3 bg-lily rounded-full animate-bounce"></div>
         <div className="w-3 h-3 bg-lily rounded-full animate-bounce delay-200"></div>
