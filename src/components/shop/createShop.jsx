818304f561e483de9240325e354c1de45270f436
Mkzay <80535194+Mkzay@users.noreply.github.com>
2025-03-31 14:49:14 +0100

    ...

--- src/components/shop/createShop.jsx
+++ src/components/shop/createShop.jsx
@@ -220,7 +220,7 @@ const CreateShop = () => {
   };
 
   return (
-    <section className="mt-10 min-h-screen flex flex-col px-4 md:px-7 gap-5 md:gap-7 items-center max-w-4xl mx-auto overflow-hidden">
+    <section className="mt-10 mb-24 min-h-screen flex flex-col px-4 md:px-7 gap-5 md:gap-7 items-center max-w-4xl mx-auto overflow-hidden">
       <div className="w-full">
         <div className="rounded-2xl border-[1px] border-solid border-black h-16 w-full flex items-center justify-center">
           <h1 className="text-xl font-normal font-poppins">
