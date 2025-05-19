f1b96054546fd324d9d89b8f194bf45c7281b6ba
Mkzay <80535194+Mkzay@users.noreply.github.com>
2025-05-19 21:16:01 +0100

    SEO

--- src/pages/shopDetails.jsx
+++ src/pages/shopDetails.jsx
@@ -1,10 +1,24 @@
 import ShopDetails from "../components/shop/shopDetails";
+import SEO from "../components/common/SEO";
 
 const productDetails = () => {
   return (
-    <section>
-      <ShopDetails />
-    </section>
+    <>
+      <SEO
+        title={`${shop?.name || "Shop"} - Lily Shops`}
+        description={
+          shop?.description ||
+          "Visit this shop on Lily Shops to discover their products and services."
+        }
+        keywords={`${shop?.name}, ${shop?.category}, local shop, Lily Shops`}
+        ogImage={shop?.image_url}
+        ogUrl={`https://lilyshops.com/shop/${shop?.id}`}
+        type="business.business"
+      />
+      <section>
+        <ShopDetails />
+      </section>
+    </>
   );
 };
 
