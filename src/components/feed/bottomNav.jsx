2b7003c0249cf0d6d9a5f02f3853498600b54033
Jamaldeen <jd33n27@gmail.com>
2025-10-14 14:40:36 +0100

    No integration yet, still have a long way to

--- src/components/feed/bottomNav.jsx
+++ src/components/feed/bottomNav.jsx
@@ -3,138 +3,128 @@ import { Link } from "react-router-dom";
 
 const BottomNav = ({ activePage, setActivePage }) => {
   return (
-    <div className="flex justify-around items-center bg-[#FFFAE7] h-12 shadow-inner fixed bottom-0 left-0 w-full z-50">
-      {/* Home  */}
-      <Link
-        to="/feed"
-        className={`flex flex-col items-center relative ${
-          activePage === "home" ? "text-lily" : "text-ash"
-        }`}
-      >
-        <button
-          onClick={() => setActivePage("home")}
-          className={`${
-            activePage === "home"
-              ? "grid place-items-center bg-lily size-10 rounded-full absolute -top-7 transform -translate-x-1/2 left-1/2"
-              : "grid place-items-center size-10 absolute -top-3 transform -translate-x-1/2 left-1/2"
+    <div className="flex items-center fixed bottom-0 h-16 bg-main left-0 w-full z-50">
+      <div className="flex items-center justify-around w-lg mx-auto sm:w-4xl">
+        {/* Home  */}
+        <Link
+          to="/feed"
+          className={`flex flex-col items-center relative ${
+            activePage === "home" ? "text-lily" : "text-ash"
           }`}
         >
-          <img
-            src={
-              activePage === "home"
-                ? "./home-icon-active.svg"
-                : "./home-icon.svg"
-            }
-            alt=""
-            className="size-7"
-          />
-        </button>
-        <span className="text-xs font-poppins mt-5">Home</span>
-      </Link>
-      {/* Shops */}
-      <Link
-        to="/shop"
-        className={`flex flex-col items-center relative ${
-          activePage === "shop" ? "text-lily" : "text-ash"
-        }`}
-      >
-        <button
-          onClick={() => setActivePage("shop")}
-          className={`${
-            activePage === "shop"
-              ? "grid place-items-center bg-lily size-10 rounded-full absolute -top-7 transform -translate-x-1/2 left-1/2"
-              : "grid place-items-center size-10 absolute -top-3 transform -translate-x-1/2 left-1/2"
+          <button
+            onClick={() => setActivePage("home")}
+            className="grid place-items-center size-7"
+          >
+            <img
+              src={
+                activePage === "home"
+                  ? "./icons/home-active.svg"
+                  : "./icons/home-4.svg"
+              }
+              alt=""
+              className="size-7"
+            />
+          </button>
+          <span className="text-xs font-poppins">Home</span>
+        </Link>
+
+        {/* Shops */}
+        <Link
+          to="/shop"
+          className={`flex flex-col items-center relative ${
+            activePage === "shop" ? "text-lily" : "text-ash"
           }`}
         >
-          <img
-            src={activePage === "shop" ? "./shop-active.svg" : "./shop.svg"}
-            alt=""
-            className="size-7"
-          />
-        </button>
-        <span className="text-xs font-poppins mt-5">Shops</span>
-      </Link>
-      {/* Create */}
-      <Link
-        to="/create"
-        className={`flex flex-col items-center relative ${
-          activePage === "create" ? "text-lily" : "text-ash"
-        }`}
-      >
-        <button
-          onClick={() => setActivePage("create")}
-          className={`${
-            activePage === "create"
-              ? "grid place-items-center bg-lily size-10 rounded-full absolute -top-7 transform -translate-x-1/2 left-1/2"
-              : "grid place-items-center size-10 absolute -top-3 transform -translate-x-1/2 left-1/2"
+          <button
+            onClick={() => setActivePage("shop")}
+            className="grid place-items-center size-7"
+          >
+            <img
+              src={
+                activePage === "shop"
+                  ? "./icons/shop-active.svg"
+                  : "./icons/shop.svg"
+              }
+              alt=""
+              className="size-7"
+            />
+          </button>
+          <span className="text-xs font-poppins">Shops</span>
+        </Link>
+
+        {/* Create */}
+        <Link
+          to="/create"
+          className={`flex flex-col items-center relative ${
+            activePage === "create" ? "text-lily" : "text-ash"
           }`}
         >
-          <img
-            src={
-              activePage === "create"
-                ? "./create-icon-active.svg"
-                : "./create-icon.svg"
-            }
-            alt=""
-            className="size-7"
-          />
-        </button>
-        <span className="text-xs font-poppins mt-5">Create</span>
-      </Link>
-      {/* Chatroom */}
-      <Link
-        to="/chat"
-        className={`flex flex-col items-center relative ${
-          activePage === "chat" ? "text-lily" : "text-ash"
-        }`}
-      >
-        <button
-          onClick={() => setActivePage("chat")}
-          className={`${
-            activePage === "chat"
-              ? "grid place-items-center bg-lily size-10 rounded-full absolute -top-7 transform -translate-x-1/2 left-1/2"
-              : "grid place-items-center size-10 absolute -top-3 transform -translate-x-1/2 left-1/2"
+          <button
+            onClick={() => setActivePage("create")}
+            className="grid place-items-center size-7"
+          >
+            <img
+              src={
+                activePage === "create"
+                  ? "./icons/create-circle-active.svg"
+                  : "./icons/create-circle.svg"
+              }
+              alt=""
+              className="size-7"
+            />
+          </button>
+          <span className="text-xs font-poppins">Create</span>
+        </Link>
+
+        {/* Chatroom */}
+        <Link
+          to="/chat"
+          className={`flex flex-col items-center relative ${
+            activePage === "chat" ? "text-lily" : "text-ash"
           }`}
         >
-          <img
-            src={
-              activePage === "chat"
-                ? "./chat-icon-active.svg"
-                : "./chat-icon.svg"
-            }
-            alt=""
-            className="size-7"
-          />
-        </button>
-        <span className="text-xs font-poppins mt-5">Chatroom</span>
-      </Link>
-      {/* Profile */}
-      <Link
-        to="/profile"
-        className={`flex flex-col items-center relative ${
-          activePage === "profile" ? "text-lily" : "text-ash"
-        }`}
-      >
-        <button
-          onClick={() => setActivePage("profile")}
-          className={`${
-            activePage === "profile"
-              ? "grid place-items-center bg-lily size-10 rounded-full absolute -top-7 transform -translate-x-1/2 left-1/2"
-              : "grid place-items-center size-10 absolute -top-3 transform -translate-x-1/2 left-1/2"
+          <button
+            onClick={() => setActivePage("chat")}
+            className="grid place-items-center size-7"
+          >
+            <img
+              src={
+                activePage === "chat"
+                  ? "./chat-icon-active.svg"
+                  : "./icons/message-3.svg"
+              }
+              alt=""
+              className="size-7"
+            />
+          </button>
+          <span className="text-xs font-poppins">Chatroom</span>
+        </Link>
+
+        {/* Profile */}
+        <Link
+          to="/profile"
+          className={`flex flex-col items-center relative ${
+            activePage === "profile" ? "text-lily" : "text-ash"
           }`}
         >
-          <img
-            src={
-              activePage === "profile"
-                ? "./home-icon-active.svg"
-                : "./nav-profile-icon.svg"
-            }
-            alt=""
-            className="size-7"
-          />
-        </button>
-        <span className="text-xs font-poppins mt-5">Profile</span>
-      </Link>
+          <button
+            onClick={() => setActivePage("profile")}
+            className="grid place-items-center size-7"
+          >
+            <img
+              src={
+                activePage === "profile"
+                  ? "./home-icon-active.svg"
+                  : "./icons/user.svg"
+              }
+              alt=""
+              className="size-7"
+            />
+          </button>
+          <span className="text-xs font-poppins">Profile</span>
+        </Link>
+      </div>
     </div>
   );
 };
