7654b9fdc0186d57f67318b2371e00e07bdc98b6
Jamaldeen <jd33n27@gmail.com>
2025-11-04 12:18:37 +0100

    corrected api paths

--- src/pages/AddAddressPage.jsx
+++ src/pages/AddAddressPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { addNewAddress } from "../api/checkoutApi";
+import { addNewAddress } from "../services/api";
 import { ChevronLeft, Loader2 } from "lucide-react";
 
 const AddAddressPage = () => {
