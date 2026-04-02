5e2f230437d8c08a09d2579a89ef7dd5c304bef7
Jamal <jd33n27@gmail.com>
2026-04-02 09:41:36 +0100

    added new base url and other changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "https://lily-shop-backend.onrender.com";
+  import.meta.env.VITE_API_URL || "https://api.lilyshops.com"; //old base-url: //lily-shop-backend.onrender.com
 
 const api = axios.create({
   baseURL: API_BASE_URL,
